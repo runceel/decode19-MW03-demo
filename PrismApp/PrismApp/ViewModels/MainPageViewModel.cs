6ca9d3161744f9eeab7960f67560821512bf4988
Kazuki Ota <kaota@microsoft.com>
2019-05-28 21:22:36 +0900

    impl vm

--- PrismApp/PrismApp/ViewModels/MainPageViewModel.cs
+++ PrismApp/PrismApp/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using PrismApp.Entities;
+using PrismApp.UseCases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,47 @@ namespace PrismApp.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService)
+        public IEmployeeUseCase EmployeeUseCase { get; }
+
+        public MainPageViewModel(INavigationService navigationService, IEmployeeUseCase employeeUseCase)
             : base(navigationService)
         {
             Title = "Main Page";
+            EmployeeUseCase = employeeUseCase ?? throw new ArgumentNullException(nameof(employeeUseCase));
         }
+
+        private DelegateCommand _lookupCommand;
+        public DelegateCommand LookupCommand =>
+            _lookupCommand ?? (_lookupCommand = new DelegateCommand(ExecuteLookupCommand, CanExecuteLookupCommand)
+                .ObservesProperty(() => EmployeeId));
+
+        private DelegateCommand _navigateToNextPageCommand;
+        public DelegateCommand NavigateToNextCommand =>
+            _navigateToNextPageCommand ?? (_navigateToNextPageCommand = new DelegateCommand(ExecuteNavigateToNextCommand, CanExecuteNavigateToNextCommand)
+                .ObservesProperty(() => EmployeeUseCase.Employee));
+
+        private async void ExecuteNavigateToNextCommand()
+        {
+            await NavigationService.NavigateAsync("NextPage");
+        }
+
+        private bool CanExecuteNavigateToNextCommand() => EmployeeUseCase.Employee != null;
+
+        private async void ExecuteLookupCommand()
+        {
+            EmployeeUseCase.TargetId = int.Parse(EmployeeId);
+            await EmployeeUseCase.LookupAsync();
+        }
+
+        private bool CanExecuteLookupCommand() => ValidateInputs();
+
+        private string _employeeId;
+        public string EmployeeId
+        {
+            get => _employeeId;
+            set => _ = SetProperty(ref _employeeId, value);
+        }
+
+        private bool ValidateInputs() => int.TryParse(EmployeeId, out var _);
     }
 }
