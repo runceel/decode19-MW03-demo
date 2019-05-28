136c5d61b433317f1f289f5bdbab71d9ed3b3872
Kazuki Ota <kaota@microsoft.com>
2019-05-28 21:06:30 +0900

    add employee

---
+++ PrismApp/PrismApp/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrismApp.Entities
+{
+    public class Employee
+    {
+        public Employee()
+        {
+        }
+
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PrismApp/PrismApp/UseCases/IEmployeeUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrismApp.UseCases
+{
+    public interface IEmployeeUseCase
+    {
+        int TargetId { get; set; }
+    }
+}
